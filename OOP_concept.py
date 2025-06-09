1d5fc52a6ac2bc161bb8cf8b8e1fe259ee87a84c
Jagadeesh <johndoe@gmail.com>
2025-06-09 22:46:04 +0530

    new OOP

---
+++ OOP_concept.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        print("Hello, my name is", self.name)
+
+
+# Create a student object and call the greet method
+student1 = Student("Jagadeesh")
+student1.greet()
